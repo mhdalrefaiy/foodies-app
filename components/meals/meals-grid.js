caaff2037813fb8f7180fd9656e19ac6e5029a75
john.markson <john.markson@itm-development.com>
2025-11-21 00:08:49 +0300

    fix errors

--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -5,6 +5,7 @@ export default function MealsGrid({meals}) {
     return (
         <ul className={classes.meals} >
             {meals.map((meal) => 
+            
                 <li key={meal.id} >
                     <MealItem {...meal} />
                 </li>
